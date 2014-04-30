5761623be41894222ea7f92e22b852fcf50ed73f
Bartosz Firyn <songo.bercik@interia.pl>
2014-04-30 15:39:09 +0000

    Performance problem in zTree init with 1k nodes

--- js/jquery.ztree.exhide-3.5.js
+++ js/jquery.ztree.exhide-3.5.js
@@ -281,17 +281,21 @@
 //	Override method in core
 	var _dInitNode = data.initNode;
 	data.initNode = function(setting, level, node, parentNode, isFirstNode, isLastNode, openFlag) {
-		var tmpPNode = (parentNode) ? parentNode: data.getRoot(setting),
-			children = tmpPNode[setting.data.key.children];
-		data.tmpHideFirstNode = view.setFirstNodeForHide(setting, children);
-		data.tmpHideLastNode = view.setLastNodeForHide(setting, children);
-		view.setNodeLineIcos(setting, data.tmpHideFirstNode);
-		view.setNodeLineIcos(setting, data.tmpHideLastNode);
-		isFirstNode = (data.tmpHideFirstNode === node);
-		isLastNode = (data.tmpHideLastNode === node);
+		if (openFlag) {
+			var tmpPNode = (parentNode) ? parentNode: data.getRoot(setting),
+				children = tmpPNode[setting.data.key.children];
+			data.tmpHideFirstNode = view.setFirstNodeForHide(setting, children);
+			data.tmpHideLastNode = view.setLastNodeForHide(setting, children);
+			view.setNodeLineIcos(setting, data.tmpHideFirstNode);
+			view.setNodeLineIcos(setting, data.tmpHideLastNode);
+			isFirstNode = (data.tmpHideFirstNode === node);
+			isLastNode = (data.tmpHideLastNode === node);
+		}
 		if (_dInitNode) _dInitNode.apply(data, arguments);
-		if (isLastNode) {
-			view.clearOldLastNode(setting, node);
+		if (openFlag) {
+			if (isLastNode) {
+				view.clearOldLastNode(setting, node);
+			}
 		}
 	};
 
