9529b9e1bcdfe52a571ab123afee2c8c09cadea8
ztree <hunter.z@263.net>
2017-01-20 12:28:50 +0800

    fixed issue:  scrollIntoViewIfNeeded will throw error , because IE8 has not HTMLElement

--- js/jquery.ztree.exedit.js
+++ js/jquery.ztree.exedit.js
@@ -1,5 +1,5 @@
 /*
- * JQuery zTree exedit v3.5.27
+ * JQuery zTree exedit v3.5.28
  * http://treejs.cn/
  *
  * Copyright (c) 2010 Hunter.z
@@ -8,7 +8,7 @@
  * http://www.opensource.org/licenses/mit-license.php
  *
  * email: hunter.z@263.net
- * Date: 2016-12-27
+ * Date: 2017-01-20
  */
 (function($){
 	//default consts of exedit
