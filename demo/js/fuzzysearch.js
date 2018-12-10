aed5f2210f76c34f0b2fd84acb2c6dc8f1a4332a
zTree-github <hunter.z@263.net>
2018-12-10 16:01:00 +0800

    fixed issue: fuzzySearch.js & Demo   Thanks @SadWood & @JerryWang24

--- demo/js/fuzzysearch.js
+++ demo/js/fuzzysearch.js
@@ -110,6 +110,7 @@
 	});
 
 	var timeoutId = null;
+  var lastKeyword = '';
 	// excute lazy load once after input change, the last pending task will be cancled  
 	function searchNodeLazy(_keywords) {
 		if (timeoutId) { 
@@ -117,8 +118,12 @@
 			clearTimeout(timeoutId);
 		}
 		timeoutId = setTimeout(function() {
+      if (lastKeyword === _keywords) {
+        return;
+      }
 			ztreeFilter(zTreeObj,_keywords); //lazy load ztreeFilter function 
-			$(searchField).focus();//focus input field again after filtering
+			// $(searchField).focus();//focus input field again after filtering
+      lastKeyword = _keywords;
 		}, 500);
 	}
 }
